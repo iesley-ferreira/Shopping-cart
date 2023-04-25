18e05a5b584fc55130cda86d130cdb1146e19ad1
iesley-ferreira <iesley_ferreira@hotmail.com>
2023-04-25 18:30:45 -0300

    feat: cria lista de produtos

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-console.log(await fetchProductsList('computador'));
+const productList = await fetchProductsList('computador');
+
+productList.map(async (product) => {
+  const list = await createProductElement(product);
+  document.querySelector('.products').appendChild(list);
+});
